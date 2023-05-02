12329a0bdb2851411936184f201be4727e9f2c33
Dan Kiuna <dkkiuna11@gmail.com>
2023-05-02 11:40:07 -0500

    start

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,15 @@ import App from './App'
 import './index.css'
 
 import { Amplify } from 'aws-amplify'
-import config from './aws-exports'
-Amplify.configure(config)
+// import config from './aws-exports'
+Amplify.configure({
+  Auth: {
+    identityPoolId: 'us-east-1:70efeb97-3df8-4ecc-b1ec-e9cbcdeea891',
+    region: 'us-east-1',
+    userPoolId: `us-east-1_OgI9b3GOP`,
+    userPoolWebClientId: '1g3rltbnh4f1pe457d2sfeff8s',
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
